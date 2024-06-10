cf3597fea5ad16096ae2209f898e837dbd5e2538
Beverly Ifeobu <beverlyifeobu@gmail.com>
2024-06-10 07:54:22 -0400

    made Comment schema

--- models/Comment.js
+++ models/Comment.js
@@ -3,5 +3,11 @@ const { Schema, model } = require('mongoose');
 
 
 const commentSchema = new Schema({
-    
+    content: {
+        type: String,
+    },
+    author: {
+        type: String,
+        required: true
+    }
 })
