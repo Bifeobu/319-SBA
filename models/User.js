dec55bba8d2d7d4d59b17c5fe24d7ac01a2b8a7d
Beverly Ifeobu <beverlyifeobu@gmail.com>
2024-06-10 08:42:38 -0400

    update on user schema

--- models/User.js
+++ models/User.js
@@ -1,6 +1,14 @@
 const { Schema, model } = require('../config/db-connection');
 
 const userSchema = Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  job_title: {
+    type: String,
+    required: true,
+  },
   email: {
     type: String,
     required: true,
