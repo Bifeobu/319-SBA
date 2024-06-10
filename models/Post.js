ea0fdbc0cbd0f9645e1a795f777b90f97875e595
Beverly Ifeobu <beverlyifeobu@gmail.com>
2024-06-10 08:06:21 -0400

    updated Post schema

--- models/Post.js
+++ models/Post.js
@@ -3,5 +3,19 @@ const { Schema, model } = require('mongoose');
 
 
 const postSchema = new Schema({
-    
+    title: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+        min: 4,
+        max: 500,
+    },
+    author: {
+        type: String,
+        required: true,
+    }
+
 })
