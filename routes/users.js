62342d284214b006910e6f87b5d8cf30c40ce250
Beverly Ifeobu <beverlyifeobu@gmail.com>
2024-06-11 00:51:55 -0400

    made comments

--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,10 @@ const usersCtrl = require('../controllers/users.js');
 
 const router = Router();
 
+//user post route
 router.post('/', usersCtrl.createUser);
 
+//users get route
 router.get('/', usersCtrl.getUsers);
 
 module.exports = router;
