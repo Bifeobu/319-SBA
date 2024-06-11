aec40cd01df560a2c2cb09b115dd605d8c74f118
Beverly Ifeobu <beverlyifeobu@gmail.com>
2024-06-11 08:04:31 -0400

    requiring all data models

--- config/seed.js
+++ config/seed.js
@@ -1,2 +1,6 @@
 //require connection file
 const mongoose = require('./db-connection.js');
+//requiring data models
+const User = require('../models/User.js');
+const Post = require('../models/Post.js');
+const Comment = require('../models/Comment.js');
